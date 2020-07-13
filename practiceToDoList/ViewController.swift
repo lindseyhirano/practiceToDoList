4f36cf5e75bb75ce181bbefe83526a5c0f7fdecb
Lindsey Hirano <lindseysh11@gmail.com>
2020-07-13 15:29:58 -0700

    Initial Commit

---
+++ practiceToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practiceToDoList
+//
+//  Created by lindsey hirano on 7/13/20.
+//  Copyright © 2020 Lindsey Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
